b009caa43bac136f91aee085516e25e0bd2f3230
Sneara0 <snearaparvin.cse@gmail.com>
2025-12-31 15:32:05 +0600

    add auth new things

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express from 'express';
 import { postRouter } from './modules/post/post.router';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
-
+import cors from 'cors';
 const app=express();
+
+
+app.use (cors({
+    origin:process.env.App_URL! || 'http://localhost:4000',
+    credentials:true,
+
+}))
 app.use(express.json());
 app.all('/api/auth/*splat', toNodeHandler(auth));
 app.post('/posts',postRouter)
